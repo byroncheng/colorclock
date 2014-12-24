e7e0574fb0433f059d8c649270f4e0b8515bcdcf
Byron Cheng <byroncheng@gmail.com>
2014-12-23 17:08:09 -0800

    added clock, color chcnaging in progress

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,5 +1,29 @@
 //DOM Ready =======================
 $(document).ready(function(){
 
+	setInterval(function(){
+		var date = new Date();
+
+		var hours = pad(date.getHours());
+		var minutes = pad(date.getMinutes());
+		var seconds = pad(date.getSeconds());
+		var millisecond = date.getMilliseconds();
+
+		$('#time').html(hours+':'+minutes+':'+seconds);
+
+		var lum = Math.round(hours*100/24)+'%';
+		var sat = Math.round(minutes*100/60)+'%';
+		var hue = Math.round(seconds*360/60+millisecond*5/1000);
+
+		//$('#debug').append(hours+':'+minutes+':'+seconds);
+		//$('#debug').append('hue='+hue+' sat='+sat+' lum='+lum+'<br>');
+
+		document.body.style.backgroundColor=('hsl('+hue+','+sat+','+lum+')');
+	},200);
+	
 });
+
+var pad = function (value) {
+    return (value < 10 ? '0' : '') + value;
+};
 
